efeaf5e1f5277ac45c5f929b1fef4873419fb7e8
Ruben Nati <ruben.nati@me.com>
2020-06-28 18:43:02 +0200

    Remove classes components

--- src/component/RouterComponent.jsx
+++ src/component/RouterComponent.jsx
@@ -1,29 +1,24 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import ListUserComponent from "./user/ListUserComponent";
-import AddUserComponent from "./user/AddUserComponent";
-import EditUserComponent from "./user/EditUserComponent";
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import UsersList from './user/UsersList';
 import AddUser from './user/AddUser';
 import EditUser from './user/EditUser';
 
 const AppRouter = () => {
-    return(
-        <div style={style}>
-            <Router>
-                    <Switch>
-                        <Route path="/" exact component={UsersList} />
-                        <Route path="/users" component={UsersList} />
-                        <Route path="/add-user" component={AddUser} />
-                        <Route path="/edit-user" component={EditUser} />
-                    </Switch>
-            </Router>
-        </div>
-    )
+    return <div style={style}>
+        <Router>
+            <Switch>
+                <Route path="/" exact component={UsersList} />
+                <Route path="/users" component={UsersList} />
+                <Route path="/add-user" component={AddUser} />
+                <Route path="/edit-user" component={EditUser} />
+            </Switch>
+        </Router>
+    </div>;
 }
 
-const style={
-    marginTop:'20px'
+const style = {
+    marginTop: '20px'
 }
 
 export default AppRouter;
